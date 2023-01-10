837a657cb94e128f88c6a7a383e068d7ea9fabe4
v833 <2507301541@qq.com>
2023-01-10 19:42:01 +0800

    feat: add google fonts

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ class App extends Component {
       return item.name.toLocaleLowerCase().includes(searchField)
     })
     return (
-      <main>
+      <main className={'App'}>
+        <h1 className={'app-title'}>Monsters Rolodex</h1>
         <SearchBox
           className={'monsters-search-box'}
           onChangeHandler={onSearchChange}
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Card from '../card/card.component'
 import './card-list.styles.css'
 
 class CardList extends Component {
@@ -9,17 +10,7 @@ class CardList extends Component {
     return (
       <div className={'card-list'}>
         {monsters.map((monster) => {
-          const { name, email, id } = monster
-          return (
-            <div className={'card-container'} key={id}>
-              <img
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                alt={`monster ${name}`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          )
+          return <Card monster={monster} key={monster.id}></Card>
         })}
       </div>
     )
